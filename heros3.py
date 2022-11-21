ce61a11f99bdfbfd6f688b807d7701cee710f06d
dlgkwls <yihajin0916@gmail.com>
2022-11-21 15:50:59 -0500

    Added rounds and pics

--- heros3.py
+++ heros3.py
@@ -12,12 +12,46 @@ class GameState():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                self.state = 'main_game'  
+                self.state = 'round_one'  
 
         #drawing
         pygame.image.load("intro.jpg")
         screen.blit(background,(0,0))
         screen.blit(ready_text,(screen_width/2 - 115, screen_height/2 - 33))
+        player_group.draw(screen)
+        player_group.update()
+ 
+        pygame.display.flip()
+
+    def round_one(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.state = 'round_two'  
+
+        #drawing
+        pygame.image.load("round one.png")
+        screen.blit(background,(0,0))
+        screen.blit(start_text,(screen_width/2, screen_height/2))
+        player_group.draw(screen)
+        player_group.update()
+ 
+        pygame.display.flip()
+
+    def round_two(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.state = 'main_game'
+
+        #drawing
+        pygame.image.load("round two.png")
+        screen.blit(background,(0,0))
+        screen.blit(start_text,(screen_width/2, screen_height/2))
         player_group.draw(screen)
         player_group.update()
  
@@ -41,6 +75,10 @@ class GameState():
             #ytgyj
         if self.state == 'main_game':
             self.main_game()
+        if self.state == 'round_one':
+            self.round_one()
+        if self.state == 'round_two':
+            self.round_two()
 
         
 #General Setup
@@ -60,6 +98,7 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Hi")
 background = pygame.image.load("background.jpg")
 ready_text = pygame.image.load("ready.jpg")
+start_text = pygame.image.load("start.png")
 
 player_group = pygame.sprite.Group()
 
