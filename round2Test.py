2bb97a9ae74284ab55cb603360a2d3c358d08c06
choihyoeun04 <choihyoeun04@gmail.com>
2022-11-25 08:42:02 -0500

    deleted jump

--- round2Test.py
+++ round2Test.py
@@ -183,22 +183,7 @@ while run:
         man.standing = True
         man.walkCount = 0
         
-    if not(man.isJump):
-        if keys[pygame.K_UP]:
-            man.isJump = True
-            man.right = False
-            man.left = False
-            man.walkCount = 0
-    else:
-        if man.jumpCount >= -10:
-            neg = 1
-            if man.jumpCount < 0:
-                neg = -1
-            man.y -= (man.jumpCount ** 2) * 0.5 * neg
-            man.jumpCount -= 1
-        else:
-            man.isJump = False
-            man.jumpCount = 10
+    
             
     redrawGameWindow()
 
