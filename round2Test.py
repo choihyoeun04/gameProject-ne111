3f84220d0022745146735524f15dce381cdcff39
choihyoeun04 <choihyoeun04@gmail.com>
2022-11-27 21:33:35 -0500

    Health bar added to monster

--- round2Test.py
+++ round2Test.py
@@ -80,20 +80,24 @@ class enemy(object):
         self.walkCount = 0
         self.vel = 3
         self.hitbox = (self.x + 17, self.y + 2, 31, 57)
+        self.health = 10 
+        self.visible = True 
 
     def draw(self,win):
         self.move()
-        if self.walkCount + 1 >= 33:
-            self.walkCount = 0
+        if self.visible:
+            if self.walkCount + 1 >= 33:
+                self.walkCount = 0
 
-        if self.vel > 0:
-            win.blit(self.walkRight[self.walkCount //3], (self.x, self.y))
-            self.walkCount += 1
-        else:
-            win.blit(self.walkLeft[self.walkCount //3], (self.x, self.y))
-            self.walkCount += 1
-        self.hitbox = (self.x + 17, self.y + 2, 31, 57)
-        pygame.draw.rect(win, (255,0,0), self.hitbox,2)
+            if self.vel > 0:
+                win.blit(self.walkRight[self.walkCount //3], (self.x, self.y))
+                self.walkCount += 1
+            else:
+                win.blit(self.walkLeft[self.walkCount //3], (self.x, self.y))
+                self.walkCount += 1
+            self.hitbox = (self.x + 17, self.y + 2, 31, 57)
+            pygame.draw.rect(win, (255,0,0), (self.hitbox[0], self.hitbox[1] - 20, 50, 10)) # NEW
+            pygame.draw.rect(win, (0,128,0), (self.hitbox[0], self.hitbox[1] - 20, 50 - (5 * (10 - self.health)), 10)) # NEW
 
     def move(self):
         if self.vel > 0:
@@ -109,7 +113,11 @@ class enemy(object):
                 self.vel = self.vel * -1
                 self.walkCount = 0
 
-    def hit(self):
+    def hit(self): 
+        if self.health > 0:
+            self.health -= 1
+        else:
+            self.visible = False
         print('hit')
 
         
@@ -150,7 +158,7 @@ while run:
                 
         if bullet.x < 500 and bullet.x > 0:
             bullet.x += bullet.vel
-        else:
+        else: 
             bullets.pop(bullets.index(bullet))
 
     keys = pygame.key.get_pressed()
@@ -159,7 +167,7 @@ while run:
         if man.left:
             facing = -1
         else:
-            facing = 1
+            facing = 1 
             
         if len(bullets) < 5:
             bullets.append(projectile(round(man.x + man.width //2), round(man.y + man.height//2), 6, (0,0,0), facing))
